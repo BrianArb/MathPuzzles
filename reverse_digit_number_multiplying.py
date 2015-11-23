9949bbcbb2766cb37e569f8a143fa000189948d2
Brian Arb <brianjamesarb@gmail.com>
2015-11-22 22:19:25 -0500

    Added docstrings

--- reverse_digit_number_multiplying.py
+++ reverse_digit_number_multiplying.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python
+"""
+Number Theory: Reverse digits by multiplying by 4
+
+Find two five-digit numbers such that
+one is 4 times the other, and the digits
+of the first are the reverse of the digits of the second.
+In other words, ABCDE * 4 = EDCBA?
+"""
 
 def reversable_product(n):
+  """Find two five-digit numbers.
+
+  Args:
+    n: int, the factor to multiply by.
+  Returns:
+    list the factor when reversed matches the product.
+  """
   numbers = []
   for x in range(10000, 99999+1):
     a = x * n
@@ -10,8 +25,12 @@ def reversable_product(n):
 
 
 def main():
+  """
+  Find two five-digit numbers with factors from 2 to 99.
+  """
+
   numbers = []
-  for n in range(2, 99):
+  for n in range(2, 100):
     ans = reversable_product(n)
     if ans:
       numbers.append(tuple([n, ans[0], n * ans[0]]))
